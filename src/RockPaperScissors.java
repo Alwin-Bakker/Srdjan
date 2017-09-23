4b1b7cac7247b8ed61c1ecd7741a879ae67c3f8b
Unknown <alwibakk@hotmail.nl>
2017-09-23 17:47:25 +0200

    update since hangouts

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -2,10 +2,13 @@ import java.util.*;
 
 public class RockPaperScissors {
     public static void main(String[] args) {
+        String staticComputer = ComputerPlay();
         Scanner scanner = new Scanner(System.in);
         System.out.println("What is your move?");
         String move = scanner.nextLine();
-        ComputerPlay();
+        convert(move);
+        mortalKombat(move, staticComputer);
+        //battleReport(move, staticComputer);
 
     }
 
@@ -13,32 +16,104 @@ public class RockPaperScissors {
         Random hitme = new Random();
         int newint = hitme.nextInt(5);
         if (newint == 0) {
-           return  "Rock";
+            return "Rock";
         }
         if (newint == 1) {
-            return  "Paper";
+            return "Paper";
         }
         if (newint == 2) {
-            return  "Scissors";
+            return "Scissors";
         }
         if (newint == 3) {
-            return  "Spock";
-        }
-        else {
-            return  "Lizard";
+            return "Spock";
+        } else {
+            return "Lizard";
         }
     }
 
-    public static String mortalKombat(String move, String ComputerPlay){
-    if (move == "r" || move == "R" || move == "Rock" || move == "rock"){
-        if (ComputerPlay == "Scissors" || ComputerPlay == "Lizard"){
-            System.out.println("Player wins!");
+    public static String convert (String move){
+        if (move == "r" || move == "R" || move == "Rock" || move == "rock"){
+            return "Rock";
         }
-        if (ComputerPlay == "Paper" || ComputerPlay == "Spock"){
-            System.out.println("Computer wins!");
+        if (move == "p" || move == "P" || move == "Paper" || move == "paper") {
+            return "Paper";
         }
-        else {
-            System.out.println("This is a draw");
+        if (move == "s" || move == "S" || move == "Scissors" || move == "scissors"){
+            return "Scissors";
         }
+        if (move == "l" || move == "L" || move == "Lizard" || move == "lizard") {
+            return "Lizard";
+        }
+        if (move == "o" || move == "O" || move == "Spock" || move == "spock") {
+           return "Spock";
+        }
+        else{
+            System.out.println("this is not a legal input, I will pick Rock for you");
+            return "Rock";
+        }
+
+
     }
-    }
+
+    public static void mortalKombat(String move, String ComputerPlay) {
+        if (move == "Rock") {
+            if (ComputerPlay == "Scissors" || ComputerPlay == "Lizard") {
+                System.out.println("Player wins!");
+            }
+            if (ComputerPlay == "Paper" || ComputerPlay == "Spock") {
+                System.out.println("Computer wins!");
+            } else {
+                System.out.println("This is a draw");
+            }
+        }
+        if (move == "Paper") {
+            if (ComputerPlay == "Spock" || ComputerPlay == "Rock") {
+                System.out.println("Player wins!");
+            }
+            if (ComputerPlay == "Scissors" || ComputerPlay == "Lizard") {
+                System.out.println("Computer wins!");
+            } else {
+                System.out.println("This is a draw");
+            }
+        }
+
+        if (move == "Scissors") {
+            if (ComputerPlay == "Lizard" || ComputerPlay == "Paper") {
+                System.out.println("Player wins!");
+            }
+            if (ComputerPlay == "Rock" || ComputerPlay == "Spock") {
+                System.out.println("Computer wins!");
+            } else {
+                System.out.println("This is a draw");
+            }
+        }
+        if (move == "Lizard") {
+            if (ComputerPlay == "Paper" || ComputerPlay == "Spock") {
+                System.out.println("Player wins!");
+            }
+            if (ComputerPlay == "Scissors" || ComputerPlay == "Rock") {
+                System.out.println("Computer wins!");
+            } else {
+                System.out.println("This is a draw");
+            }
+        }
+        if (move =="Spock") {
+            if (ComputerPlay == "Scissors" || ComputerPlay == "rock") {
+                System.out.println("Player wins!");
+            }
+            if (ComputerPlay == "Paper" || ComputerPlay == "Lizard") {
+                System.out.println("Computer wins!");
+            } else {
+                System.out.println("This is a draw");
+            }
+        }
+    }}
+
+   // public static void battleReport(String move, String Computerplay ){
+   //     if (move == "Rock" || Computerplay == "Rock")
+//
+
+   // }
+
+   // }
+
