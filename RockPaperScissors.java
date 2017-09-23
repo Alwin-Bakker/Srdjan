586f72dae746fbeee0c2a610a3eae842f8c58f63
alwibakk <alwibakk@hotmail.nl>
2017-09-23 16:15:52 +0200

    Add files via upload

---
+++ RockPaperScissors.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class RockPaperScissors {
+    public static void main (String[] args){
+
+    }
+
+   // public static String ComputerPlay(int x){
+    //    Random hitme = new Random();
+   //     int newint = hitme.nextInt(5);
+  //      if (newint == 0) {
+       //     ComputerPlay = "Rock";
+        }
+      //  return ComputerPlay;}
+
+//}
+
